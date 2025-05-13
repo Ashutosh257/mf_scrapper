cae1f294b79258af1a93ecbc66abb8b4f5b566f8
Ashutosh Sanjeev Bhujbal <theflash257@gmail.com>
2025-05-13 22:16:10 +0100

    point selenium to the chromium binary

--- scrape_nav.py
+++ scrape_nav.py
@@ -10,6 +10,8 @@ options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
+options.binary_location = "/usr/bin/chromium-browser"
+
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.etmoney.com/mutual-funds/filter/latest-mutual-fund-nav")
 time.sleep(6)
