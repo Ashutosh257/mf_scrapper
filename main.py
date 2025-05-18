eced24b53751a6afbe2fed9130c98f2553dabcb9
Ashutosh Sanjeev Bhujbal <theflash257@gmail.com>
2025-05-18 03:24:25 +0100

    change base design

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ from pathlib import Path
 
 app = FastAPI()
 
-nav_data = []  # Global variable to store the data
+nav_data = {}  # Global variable to store the data
 
 @app.on_event("startup")
 def load_nav_data():
     global nav_data
     with open("nav_data.json", "r") as f:
-        nav_data = json.load(f)
+        temp: List[dict] = json.load(f)
+    
+    for data in temp:
+        nav_data[data["scheme_id"]] = data
+
 
 
 @app.get("/")
@@ -30,73 +34,24 @@ def get_all_navs():
 @app.get("/nav/vijay")
 def get_nav_vijay():
 
-    mfs_to_filter = [
-        {
-            "scheme_id": "19232",
-            "scheme_name": "Parag Parikh Flexi Cap FundFlexi Cap",
-        },
-        {
-            "scheme_id": "21891",
-            "scheme_name": "PGIM India Midcap Opportunities FundMid Cap",
-        },
-        {
-            "scheme_id": "16144",
-            "scheme_name": "Canara Robeco Emerging Equities FundLarge & MidCap",
-        },
-        {
-            "scheme_id": "16126",
-            "scheme_name": "Mirae Asset Large & Midcap FundLarge & MidCap",
-        },
-        {
-            "scheme_id": "42175",
-            "scheme_name": "Kotak Multicap FundMulti Cap",
-        },
-        {
-            "scheme_id": "16925",
-            "scheme_name": "Quant Small Cap FundSmall Cap",
-        },
-        {
-            "scheme_id": "38680",
-            "scheme_name": "DSP Healthcare FundSectoral-Pharma",
-        },
-        {
-            "scheme_id": "39186",
-            "scheme_name": "Tata Balanced Advantage FundDynamic Asset Allocation",
-        },
-        {
-            "scheme_id": "15354",
-            "scheme_name": "SBI Small Cap FundSmall Cap",
-        },
-        {
-            "scheme_id": "38470",
-            "scheme_name": "Tata Small Cap FundSmall Cap",
-        },
-        {
-            "scheme_id": "39296",
-            "scheme_name": "Canara Robeco Small Cap FundSmall Cap",
-        },
-        {
-            "scheme_id": "40721",
-            "scheme_name": "Motilal Oswal Nifty Smallcap 250 Index FundSmall Cap Index",
-        },
-        {
-            "scheme_id": "44997",
-            "scheme_name": "Tata Nifty Capital Markets Index FundOther Equity Index",
-        },
-        {
-            "scheme_id": "45257",
-            "scheme_name": "Motilal Oswal Innovation Opportunities FundThematic",
-        }
+    vijay_mf_ids = [
+        "19232",
+        "21891",
+        "16144",
+        "16126",
+        "42175",
+        "16925",
+        "38680",
+        "39186",
+        "15354",
+        "38470",
+        "39296",
+        "40721",
+        "44997",
+        "45257"
     ]
 
-    print(len(mfs_to_filter))
-    
-    ids = [scheme["scheme_id"] for scheme in mfs_to_filter]
-
-    print(ids)
-
-
-    return ids
+    return [nav_data[_id] for _id in vijay_mf_ids if nav_data.get(_id, False)]
 
 # nav csv format
 @app.get("/nav/csv", response_class=PlainTextResponse)
@@ -113,9 +68,19 @@ def get_nav_csv():
     return csv_data
 
 
+# eg: /nav/12345
+@app.get("/nav/{scheme_id}")
+def get_nav_by_id(scheme_id: str):
+    result = nav_data.get(scheme_id)
+    if not result:
+        raise HTTPException(status_code=404, detail="No such scheme(mutual fund) exists")
+    return result
+
+
+# eg: /nav/?scheme_ids=12345&scheme_ids=67890
 @app.get("/nav/")
 def get_nav_by_ids(scheme_ids: List[str] = Query(...)):
-    results = [item for item in nav_data if item["scheme_id"] in scheme_ids]
+    results = [nav_data[_id] for _id in scheme_ids if nav_data.get(_id, False)]
     if not results:
-        raise HTTPException(status_code=404, detail="No matching schemes found")
+        raise HTTPException(status_code=404, detail="No matching schemes(mutual funds) found")
     return results
